0dac5c5197d87fc1f0d7427e6ff1a34288b056c1
huyennt1 <huyennt1@kaopiz.com>
2023-07-22 15:26:49 +0700

    update dashboard

--- src/main/java/com/hnt/dental/service/DashboardService.java
+++ src/main/java/com/hnt/dental/service/DashboardService.java
@@ -3,6 +3,7 @@ package com.hnt.dental.service;
 import com.hnt.dental.dao.*;
 import com.hnt.dental.dao.impl.*;
 import com.hnt.dental.dto.response.DashboardDto;
+import com.hnt.dental.dto.response.StatisticDto;
 import com.hnt.dental.entities.Patient;
 import com.hnt.dental.util.ServletUtils;
 import jakarta.servlet.http.HttpServletRequest;
@@ -24,14 +25,17 @@ private static final PaymentDao payment;
     }
 
     public void getAll(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        String type = req.getParameter("type") == null ? "7" : req.getParameter("type");
         int countDoctor = dao.countDoctorDashboard();
         Long countPatient = patient.countPatientDashboard();
         Long countBooking = booking.countBookingDashboard();
         Double countRevenue = payment.countRevenue();
+        List<StatisticDto> getStatisticBookingTime = booking.getStatisticBookingTime(type);
         List<DashboardDto> getBookingToday = booking.getAllBookingToday();
         String countRevenueString = String.format("%,.0f", countRevenue);
         req.setAttribute("countDoctor", countDoctor);
         req.setAttribute("countPatient", countPatient);
+        req.setAttribute("getStatisticBookingTime", getStatisticBookingTime);
         req.setAttribute("countBooking", countBooking);
         req.setAttribute("countRevenue", countRevenueString);
         req.setAttribute("getBookingToday", getBookingToday);
