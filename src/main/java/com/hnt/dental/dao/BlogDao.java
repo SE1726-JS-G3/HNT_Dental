758ad0e687e3abf5d63304a6900352b032d9a1d0
Trịnh Tứ Bảo <132968240+Baotthe163571@users.noreply.github.com>
2023-07-22 16:06:59 +0700

    update code fix_frontend

--- src/main/java/com/hnt/dental/dao/BlogDao.java
+++ src/main/java/com/hnt/dental/dao/BlogDao.java
@@ -2,6 +2,7 @@ package com.hnt.dental.dao;
 
 import com.hnt.dental.dto.response.BlogResDto;
 import com.hnt.dental.entities.Blogs;
+import com.hnt.dental.entities.CategoryBlog;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -21,6 +22,15 @@ public interface BlogDao extends GenericDao<Blogs> {
     Integer countListBlogSummary(String search,String status, String category) throws SQLException;
 
 
+    Blogs getBlogID(int id) throws Exception;
 
+    List<CategoryBlog> getListCategoryBlog() throws SQLException;
 
+    List<BlogResDto> getListBlogRelated(long id, long categoryId ) throws SQLException;
+
+    List<Blogs> RecentPosts() throws Exception;
+
+    Integer countListBlogs(String search) throws SQLException;
+
+    List<BlogResDto> getListBlogsFilter(Integer offset, Integer limit, String search, String oder, Integer categoryId ) throws SQLException;
 }
