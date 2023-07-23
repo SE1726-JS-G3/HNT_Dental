b87bfab17cb1c4745d2684c6205b404ff9cbb555
loi <loiddhe163563@fpt.edu.vn>
2023-07-23 11:55:27 +0700

    fix lại lỗi

--- src/main/java/com/hnt/dental/service/ServiceService.java
+++ src/main/java/com/hnt/dental/service/ServiceService.java
@@ -94,7 +94,112 @@ public class ServiceService {
                 "service");
         ServletUtils.requestDispatcher(req, resp, "/WEB-INF/templates/management/service/index.jsp");
     }
+    public void getServiceById(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        String id = req.getParameter("id");
+        String typeId = req.getParameter("typeId");
+        // lấy ra loại type theo id , nếu ko có type nào thì coi như id đó ko tồn tại
+        List<ServiceTypeDto> getType = dao.getTypeByServiceId(Long.valueOf(id));
+        if (getType.isEmpty()) {
+            ServletUtils.redirect(req, resp, "/service");
+        } else {
+            typeId = (typeId == null ? getType.get(0).getIdType() : Long.valueOf(typeId)).toString();
+            // nếu typeId == null thì lấy ra type đầu tiên :  typeId == null ? getType.get(0).getIdType() : Long.valueOf(typeId)
+            ServiceDetailDto serviceResDtos = dao.getServiceDetailByServiceId(Long.valueOf(id), Long.valueOf(typeId));
+
+            List<DoctorByServiceIdDto> doctorByServiceIdDtos = dao.getDoctorByServiceIdAndServiceType(Long.valueOf(id), Long.valueOf(typeId));
+
+            List<FeedbackDto> feedbacks = feedbackDao.getFeedbackService(Long.valueOf(id));
+
+            req.setAttribute("services", serviceResDtos);
+            req.setAttribute("types", getType);
+            req.setAttribute("doctors", doctorByServiceIdDtos);
+            req.setAttribute("feedbacks", feedbacks);
+            req.setAttribute("typeId", typeId);
+            req.setAttribute("id", id);
+            ServletUtils.requestDispatcher(req, resp, "/WEB-INF/templates/home/service/detail.jsp");
+
+            // bh làm cách nào để chỉnh type ở detail nó call lại vào đây là oke
+        }
+    }
+
+    public List<ServiceSearchHomeDto> getAllServiceHome() throws Exception {
+        return dao.getAllServiceHome();
+    }
+
+    public List<ServiceDetailDto> getTopService() throws Exception {
+        return dao.getTopService();
+    }
+
+    public List<ServiceTypeDto> getAllType(Long id) throws Exception {
+        return dao.getTypeByServiceId(id);
+    }
+
+    public void getServiceDetailManagement(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        String id = req.getParameter("id");
+        ServiceDetailDto getServiceDetail = dao.getServiceDetailManagementById(Long.valueOf(id));
+        List<ServiceTypeDto> getAllTypeByServiceId = dao.getTypeByServiceId(Long.valueOf(id));
+        List<ServiceTypeDto> getTypeOfServiceAvailable= dao.getTypeOfServiceAvailable(Long.valueOf(id));
+        List<ServiceManagementDto> getServiceTypeDetailManagementById = dao.getServiceTypeDetailManagementById(Long.valueOf(id));
+        List<ServiceDoctorManagementDto> getDoctorOfServiceManagementById = dao.getDoctorOfServiceManagementById(Long.valueOf(id));
+
+        getDoctorOfServiceManagementById.forEach(doctor -> {
+            try {
+                doctor.setTypes(dao.getTypeOfDoctorByDoctorId(Long.valueOf(id), doctor.getId()));
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        List<ServiceTypeDto> getALlType = dao.getALlType();
+        req.setAttribute("serviceDetail", getServiceDetail);
+        req.setAttribute("serviceTypeDetail", getServiceTypeDetailManagementById);
+        req.setAttribute("doctorOfService", getDoctorOfServiceManagementById);
+        req.setAttribute("types", getALlType);
+        req.setAttribute("getTypeOfServiceAvailable", getTypeOfServiceAvailable);
+        ServletUtils.requestDispatcher(req, resp, "/WEB-INF/templates/management/service/detail.jsp");
+    }
+
+    public void updateServiceDetailManagement(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        String id = req.getParameter("id");
+        String name = req.getParameter("name");
+        String description = req.getParameter("description");
+        String image = req.getParameter("image");
+        String status = req.getParameter("status");
+        dao.updateServiceDetailManagementById(ServiceDetailDto.builder().id(Long.valueOf(id))
+                .name(name).description(description).image(image).updateAt(LocalDateTime.now())
+                .status(Boolean.valueOf(status)).build());
+        ServletUtils.redirect(req, resp, "/management/service/detail?id=" + id);
+    }
+
+    public void createServiceDetailManagement(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        String name = req.getParameter("name");
+        String description = req.getParameter("description");
+        String image = req.getParameter("image");
+        String status = req.getParameter("status");
+        dao.save(Service.builder().name(name)
+                .description(description).image(image)
+                .createdAt(LocalDateTime.now())
+                .status(Boolean.valueOf(status)).build());
+        ServletUtils.redirect(req, resp, "/management/service");
+    }
+
+    public void updateFeeByServiceId(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        String idService= req.getParameter("id");
+        String idType= req.getParameter("idType");
+        String fee = req.getParameter("fee");
+
+        dao.updateServiceFeeByServiceTypeId(Long.valueOf(idService), Long.valueOf(idType), Double.valueOf(fee));
+        ServletUtils.redirect(req, resp, "/management/service/detail?id=" + idService);
+    }
+
+    public void updateFee(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+        String idService= req.getParameter("id");
+        String idServiceType= req.getParameter("idServiceType");
+        String fee = req.getParameter("fee");
+
+        dao.updateServiceFee(Long.valueOf(idService),Long.valueOf(idServiceType), Double.valueOf(fee));
+        ServletUtils.redirect(req, resp, "/management/service/detail?id=" + idService);
+    }
+}
 
 
-    
 
